d011b91689f83cd0a70a0bde3083be1128a43488
Suphachai Hestmark <top_hestmark@yahoo.no>
2020-08-30 16:23:09 +0200

    Had to stop for now due API Internal Server Error

--- app.js
+++ app.js
@@ -3,14 +3,14 @@ const resultsContainer = document.querySelector(".results");
 
 async function getPastLaunches() {
 
-  const response = await fetch(url)
-  const results = await response.json()
+  try {
+  
+    const response = await fetch(url);
+    const results = await response.json();
 
-  resultsContainer.innerHTML = "";
+    resultsContainer.innerHTML = "";
 
-  for (let i = 0; i < results.length; i++) {
-
-    // console.log(results[i].launch_success);
+    for (let i = 0; i < results.length; i++) {
 
     const launchDate = results[i].launch_date_utc;
     let unformattedDate = new Date(launchDate);
@@ -19,25 +19,38 @@ async function getPastLaunches() {
     const da = new Intl.DateTimeFormat('en', { day: '2-digit' }).format(unformattedDate);
 
     const rocketBadge = results[i].links.mission_patch_small;
-    const name = results[i].mission_name
+    const missionName = results[i].mission_name
     const flightNumber = results[i].flight_number;
     const date = `${mo} ${da}, ${ye}`;
     const launchSite = results[i].launch_site.site_name_long;
+    const rocketName = results[i].rocket.rocket_name;
     const launchSuccess = results[i].launch_success;
     function successFactor() {
       return launchSuccess ? `<p class="successful_message">SUCCESSFUL</p>` : `<p class="unsuccessful_message">UNSUCCESSFUL</p>`;
     }
-    
+    const details = results[i].details;
+    function detailsDescription() {
+      return details ? `<p>${details}</p>` : "";
+    }
+
+    console.log(results[i].rocket.rocket_name);
 
     resultsContainer.innerHTML +=  `<div class="result">
     <img class="badge" src="${rocketBadge}" alt="">
-    <h2 class="rocket__name">${name}</h2>
+    <h2 class="mission__name">${missionName}</h2>
     <p><b>Flight Number:</b> ${flightNumber}</p>
     <p><b>Launch Date:</b> ${date}</p>
     <p><b>Launch Site:</b> ${launchSite}</p>
+    <p><b>Rocket Name:</b> ${rocketName}</p>
     ${successFactor()}
+    ${detailsDescription()}
     </div>`
   }
+  }
+  catch(error) {
+    resultsContainer.innerHTML = displayError("An error occured when calling API")
+  }
+  
 }
 
 getPastLaunches()

83d23dd550b2045d4de2d87b7c7ba432eb8d2f98
Suphachai Hestmark <top_hestmark@yahoo.no>
2020-09-02 10:30:39 +0200

    Sorted flight in decrement order

--- app.js
+++ app.js
@@ -8,34 +8,46 @@ async function getPastLaunches() {
     const response = await fetch(url);
     const results = await response.json();
 
+    function sortByFlightNumber(property){ 
+  
+      return function(a, b){  
+        
+         if(a[property] > b[property])  
+         {return -1;  }
+         else if(a[property] < b[property])  
+           {return +1;}   
+     
+         return 0;  
+      };  
+    }
+    results.sort(sortByFlightNumber("flight_number"));
+
     resultsContainer.innerHTML = "";
 
-    for (let i = 0; i < results.length; i++) {
-
-    const launchDate = results[i].launch_date_utc;
-    let unformattedDate = new Date(launchDate);
+    results.forEach(object => {
+    
+    const launchDate = object.launch_date_utc;
+    const unformattedDate = new Date(launchDate);
     const ye = new Intl.DateTimeFormat('en', { year: 'numeric' }).format(unformattedDate);
     const mo = new Intl.DateTimeFormat('en', { month: 'long' }).format(unformattedDate);
     const da = new Intl.DateTimeFormat('en', { day: '2-digit' }).format(unformattedDate);
 
-    const rocketBadge = results[i].links.mission_patch_small;
-    const missionName = results[i].mission_name
-    let flightNumber = results[i].flight_number;
+    const rocketBadge = object.links.mission_patch_small;
+    const missionName = object.mission_name
+    const flightNumber = object.flight_number;
     const date = `${mo} ${da}, ${ye}`;
-    const launchSite = results[i].launch_site.site_name_long;
-    const rocketName = results[i].rocket.rocket_name;
-    const launchSuccess = results[i].launch_success;
-    const videoLink = results[i].links.video_link;
-    const articleLink = results[i].links.article_link;
+    const launchSite = object.launch_site.site_name_long;
+    const rocketName = object.rocket.rocket_name;
+    const launchSuccess = object.launch_success;
+    const videoLink = object.links.video_link;
+    const articleLink = object.links.article_link;
     function successFactor() {
       return launchSuccess ? `<p class="successful_message">SUCCESSFUL</p>` : `<p class="unsuccessful_message">UNSUCCESSFUL</p>`;
     }
-    const details = results[i].details;
+    const details = object.details;
     function detailsDescription() {
       return details ? `<p class="details_description">${details}</p>` : "";
     }
-
-    console.log(results[i].flight_number);
 
     resultsContainer.innerHTML +=  `<div class="result">
     <img class="badge" src="${rocketBadge}" alt="">
@@ -51,7 +63,8 @@ async function getPastLaunches() {
     <a class="external_link article_link" href="${articleLink}">ARTICLE</a>
     </div>
     </div>`
-  }
+    });
+
   }
   catch(error) {
     resultsContainer.innerHTML = displayError("An error occured when calling API")
